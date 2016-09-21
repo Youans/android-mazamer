c8daddafe0e917983190d48ecad6ecd2b882562b
Youans <youans@arkdev.net>
2016-09-21 18:53:53 +0300

    Adding basic grid view and structuring project packages.

--- app/src/main/java/org/heaven/mazamer/activities/MainActivity.java
+++ app/src/main/java/org/heaven/mazamer/activities/MainActivity.java
@@ -1,15 +1,46 @@
 package org.heaven.mazamer.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.heaven.mazamer.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    GridView grid;
+
+    static final String[] letters = new String[] {
+            "A", "B", "C", "D", "E",
+            "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O",
+            "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+
+
+        grid = (GridView) findViewById(R.id.gridView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, letters);
+
+        grid.setAdapter(adapter);
+
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        ((TextView) v).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
